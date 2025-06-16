69ca5348eff761b15e525a938d2142d9a5d4daa1
Maria <maria.patricio.silva@escola.pr.gov.br>
2025-06-16 11:49:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-// Espera o DOM carregar antes de executar
 document.addEventListener("DOMContentLoaded", function () {
     const btnAluno = document.getElementById("aluno");
     const btnPedagogia = document.getElementById("pedagogia");
 
     btnAluno.addEventListener("click", function () {
-       window.location.href = 'aluno.html'; // substitua pelo link correto
+       window.location.href = 'aluno.html'; 
     });
 
     btnPedagogia.addEventListener("click", function () {
-        window.location.href = 'pedagogia.html'; // substitua pelo link correto
+        window.location.href = 'pedagogia.html';
     });
 });
