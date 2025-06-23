cda126641d083a247835d40ec5ba3273d973d225
Maria <maria.patricio.silva@escola.pr.gov.br>
2025-06-22 21:36:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const btnAluno = document.getElementById("aluno");
+    const btnPedagogia = document.getElementById("pedagogia");
+
+    btnAluno.addEventListener("click", function () {
+       window.location.href = 'aluno.html'; 
+    });
+
+    btnPedagogia.addEventListener("click", function () {
+        window.location.href = 'pedagogia_login.html'; 
+    });
+});
